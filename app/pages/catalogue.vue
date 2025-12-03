df5012b69b2dc1909af60a4991004d48bb0f25a1
Dylan Broussard <github@deraw.dev>
2025-12-03 15:51:54 +0100

    📝 Add WIP note on catalog

--- app/pages/catalogue.vue
+++ app/pages/catalogue.vue
@@ -15,5 +15,15 @@
 <template>
   <UContainer class="space-y-10 py-8">
     <BackButton />
+
+    <p>
+      Le catalogue complet des films sera bientôt disponible. Vous pourrez prochainement
+      parcourir l’ensemble des films populaires, filtrer par genre, année ou note, et
+      explorer une base enrichie grâce à l’API TMDB.
+    </p>
+
+    <p>
+      Cette section est en cours de développement et sera mise à jour prochainement.
+    </p>
   </UContainer>
 </template>
