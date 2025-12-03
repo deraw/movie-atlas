17fbb0b7c917553584fd68630e46dfba255f447e
Dylan Broussard <github@deraw.dev>
2025-12-03 15:29:55 +0100

    ♻️ Update BackBtn redirection

--- app/components/BackButton.vue
+++ app/components/BackButton.vue
@@ -1,26 +1,13 @@
-<script setup lang="ts">
-  const router = useRouter()
-
-  const goBack = () => {
-    if (window.history.length > 1) {
-      router.back()
-      return
-    }
-
-    router.push('/')
-  }
-</script>
-
 <template>
   <UButton
     color="neutral"
     variant="ghost"
     icon="i-heroicons-arrow-left"
-    class="text-sm"
-    @click="goBack"
+    class="text-sm -mx-3"
+    to="/"
   >
     <span class="sr-only sm:not-sr-only">
-      Retour à la page précédente
+      Retour à l'accueil
     </span>
   </UButton>
 </template>
